95b45f7324c234d3d775608d5d59b18f2bfea5dc
aaaafireball <watsonc5251@gmail.com>
2022-10-14 12:16:20 -0400

    updated table and AI query

--- query.js
+++ query.js
@@ -1,4 +1,7 @@
 const API_TOKEN = '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'
+let questionJson;
+let AIAnswer;
+let categoryArray;
 
 function getAiResponse(question){
     let request = new XMLHttpRequest();
@@ -12,35 +15,35 @@ function getAiResponse(question){
             return;
         }
         let response = JSON.parse(this.response);
-        const nonNouns = ["a","the"]
+        const nonNouns = ["a ","the "]
 
         let responseWithoutQuestion = response[0].generated_text.replace(`Question 1: Please answer the following Jeopardy question,\nHost: ${question}.\nAnswer: \"what is ____\"\n`, "")
         singleAnswer = responseWithoutQuestion.split("\n")[0].replace("a.", "")
 
         nonNouns.forEach((word) => {
             if (singleAnswer.includes(word)){
-                singleAnswer = singleAnswer.replace(word, "").replace(/[^0-9a-z]/gi, '').slice(1)
+                singleAnswer = singleAnswer.replace(word.replace(" ",""), "")
             }
         })
-        console.log(singleAnswer)
-
-        if (question[0].answer === singleAnswer){
+        AIAnswer = singleAnswer.trim()
+        console.log(`the AI answer is ${AIAnswer}`)
+        console.log(`the real Answer is ${questionJson.answer}`)
+        if (questionJson.answer.toLowerCase() === AIAnswer.toLowerCase()){
             console.log("Correct")
         } else {
             console.log("Incorrect")
         }
-
     }
 
 
+
+
 }
-
-
 function getQuestion() {
     let request = new XMLHttpRequest();
     request.open("GET", "https://jservice.io/api/random?count=1", true);
     request.send()
-    let question;
+    //let test;
     request.onload = function () {
         if (request.status < 200 && request.status >= 400) {
             console.log(`Error ${request.status}: ${request.statusText}`);
@@ -50,11 +53,37 @@ function getQuestion() {
         question = JSON.parse(this.response);
         console.log(question[0].question)
 
-        getAiResponse(`The answer is: ${question[0].question}`);
+        questionJson = question[0]
+        getAiResponse(questionJson.question)
+
+
+
 
 
     }
+    //console.log(`the question is ${test}`)
 
 }
 
-getQuestion()
+
+function setUpCategories(){
+    let randomNum = 1
+    let request = new XMLHttpRequest();
+    request.open("GET", `https://jservice.io/api/categories?count=5&offset=${randomNum}`, true);
+    request.send()
+    request.onload = function () {
+        if (request.status < 200 && request.status >= 400) {
+            console.log(`Error ${request.status}: ${request.statusText}`);
+            return;
+        }
+        categoryArray = JSON.parse(this.response);
+        console.log(categoryArray)
+        categoryArray.forEach((category,index) => {
+            cat = document.querySelector("#cat"+(index+1))
+            console.log(category)
+            cat.innerHTML = category.title
+
+        })
+    }
+}
+
